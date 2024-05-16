4f3135a95d2a2f6e9b4155aca61c15b747347056
Daiva <159447502+daivakosinskaite@users.noreply.github.com>
2024-05-16 16:05:06 +0300

    Add files via upload

---
+++ src/modules/from.js
@@ -0,0 +1,12 @@
+const form = ()=>{
+    return `<div class="form-group mb-2">
+        <input type="text" class="form-control term" placeholder="Jusu adresas:">
+    </div>
+    <div class="form-group mb-2">
+        <input type="text" class="form-control result" readonly>
+    </div>
+    <button type="submit" class="btn btn-primary mb-2">Ieskoti kodo</button>
+    `
+}
+ 
+export default form
---
+++ src/modules/renderForm.js
@@ -0,0 +1,9 @@
+import form from "./from"
+const renderForm = ()=>{
+    let formElement = document.createElement('form');
+    formElement.className = "form-inline";
+    formElement.innerHTML = form();
+    document.querySelector(".card-body").appendChild(formElement);
+}
+
+export default renderForm
